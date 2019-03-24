62cfc8c791efdf808ec564cbcf0c6cbdb6abfe16
Sergey Belov <belovtorzhok@gmail.com>
2019-03-24 10:35:02 +0300

    Add Dockerfile for production

--- make-ansible.mk
+++ make-ansible.mk
@@ -1,2 +1,11 @@
 development-setup-env:
 	ansible-playbook ansible/development.yml -i ansible/development -vv
+
+ansible-vaults-encrypt:
+	ansible-vault encrypt ansible/production/group_vars/all/vault.yml
+
+ansible-vaults-decrypt:
+	ansible-vault decrypt ansible/production/group_vars/all/vault.yml
+
+ansible-vaults-edit:
+    ansible-vault edit ansible/production/group_vars/all/vault.yml
