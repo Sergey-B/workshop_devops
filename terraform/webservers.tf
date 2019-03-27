7d948e7a88f0fde8c76a450575dc40493ecdfa8e
Sergey Belov <belovtorzhok@gmail.com>
2019-03-27 21:57:02 +0300

    Add config for database migration

--- terraform/webservers.tf
+++ terraform/webservers.tf
@@ -3,15 +3,15 @@ provider "digitalocean" {
   version = "~> 1.1"
 }
 
-resource "digitalocean_droplet" "web1" {
-  image  = "ubuntu-18-04-x64"
-  name   = "web-1"
-  region = "fra1"
-  size   = "s-1vcpu-1gb"
-  ssh_keys = [
-    "${digitalocean_ssh_key.sergey_b.fingerprint}"
-  ]
-}
+#resource "digitalocean_droplet" "web2" {
+#  image  = "ubuntu-18-04-x64"
+#  name   = "web-1"
+#  region = "fra1"
+#  size   = "s-1vcpu-1gb"
+#  ssh_keys = [
+#    "${digitalocean_ssh_key.sergey_b.fingerprint}"
+#  ]
+#}
 
 
 
